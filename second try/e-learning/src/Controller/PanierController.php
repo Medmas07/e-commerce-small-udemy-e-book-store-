5e5b373a205b8e95e0637d49b748afec537c4daf
Medmas07 <mohamed.masmoudi0712@gmail.com>
2025-05-30 11:57:06 +0100

    first step in StripeService

--- second try/e-learning/src/Controller/PanierController.php
+++ second try/e-learning/src/Controller/PanierController.php
@@ -4,10 +4,13 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Entity\Panier;
+use App\Service\StripeService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
 
 final class PanierController extends AbstractController
 {
@@ -32,4 +35,38 @@ final class PanierController extends AbstractController
             'panier' => $panier,
         ]);
     }
+
+
+    #[Route('/paiement', name: 'paiement')]
+    public function paiement(StripeService $stripeService): Response
+    {
+        $session = $stripeService->createCheckoutSession([
+            'price_data' => [
+                'currency' => 'eur',
+                'unit_amount' => 2000,
+                'product_data' => [
+                    'name' => 'Nom du produit',
+                ],
+            ],
+            'quantity' => 1,
+        ], 
+        $this->generateUrl('paiement_success', [], UrlGeneratorInterface::ABSOLUTE_URL),
+        $this->generateUrl('paiement_cancel', [], UrlGeneratorInterface::ABSOLUTE_URL));
+
+        return $this->redirect($session->url, 303);
+    }
+
+    #[Route('/paiement/success', name: 'paiement_success')]
+    public function success(): Response
+    {
+        return $this->render('paiement/success.html.twig');
+    }
+
+    #[Route('/paiement/cancel', name: 'paiement_cancel')]
+    public function cancel(): Response
+    {
+        return $this->render('paiement/cancel.html.twig');
+    }
+
+
 }
